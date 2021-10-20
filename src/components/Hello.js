949efec99da6b59765a7c7700064b2cdf2c5c83d
ankijais <ankijais@in.ibm.com>
2021-10-20 11:59:32 +0530

    fixing issues

---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Hello extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                Hello There
+            </div>
+        );
+    }
+}
+
+export default Hello;
